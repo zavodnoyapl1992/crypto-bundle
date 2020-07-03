cf35c7761c8cb8863561e1ecd49e7892ca66b356
zavodnoyapl <zavodnoyapl@gmail.com>
2020-07-03 14:49:19 +0300

    Symfony version

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -26,7 +26,7 @@ class Configuration implements ConfigurationInterface
 
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('agentsib_crypto');
         // Keep compatibility with symfony/config < 4.2
         if (method_exists($treeBuilder, 'getRootNode')) {
             $rootNode = $treeBuilder->getRootNode();
